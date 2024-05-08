85f15b393ee6fc3ca4064c6bdfda70c3c10fb439
AlexanderRyblov <a.ryblov@efg.gg>
2024-05-08 16:58:56 +0300

    debug

--- src/grpcbox_subchannel.erl
+++ src/grpcbox_subchannel.erl
@@ -140,7 +140,8 @@ terminate(Reason, _State, #data{conn_pid=Pid,
 
 connect(Data=#data{conn=undefined,
                    endpoint={Transport, Host, Port, SSLOptions, ConnectionSettings}}, From, Actions) ->
-    ?LOG_INFO(#{what => debug_grpcbox_subchannels_connect_1, transport => Transport, host => Host, port => Port, ssl_options => SSLOptions, conn_settings => ConnectionSettings, actions => Actions}),
+    Stacktrace = element(2, erlang:process_info(self(), current_stacktrace)),
+    ?LOG_INFO(#{what => debug_grpcbox_subchannels_connect_1, transport => Transport, host => Host, port => Port, ssl_options => SSLOptions, conn_settings => ConnectionSettings, actions => Actions, stacktrace => Stacktrace}),
     Port2 = 50051,
     case h2_client:start_link(Transport, Host, Port2, options(Transport, SSLOptions),
                               ConnectionSettings#{garbage_on_end => true,
