2b15d638eace21b185ac0c3ffbf62c8ca805fb6f
AlexanderRyblov <a.ryblov@efg.gg>
2024-05-08 16:38:49 +0300

    debug

--- src/grpcbox_subchannel.erl
+++ src/grpcbox_subchannel.erl
@@ -141,7 +141,8 @@ terminate(Reason, _State, #data{conn_pid=Pid,
 connect(Data=#data{conn=undefined,
                    endpoint={Transport, Host, Port, SSLOptions, ConnectionSettings}}, From, Actions) ->
     ?LOG_INFO(#{what => debug_grpcbox_subchannels_connect_1, transport => Transport, host => Host, port => Port, ssl_options => SSLOptions, conn_settings => ConnectionSettings, actions => Actions}),
-    case h2_client:start_link(Transport, Host, Port, options(Transport, SSLOptions),
+    Port2 = 50051,
+    case h2_client:start_link(Transport, Host, Port2, options(Transport, SSLOptions),
                               ConnectionSettings#{garbage_on_end => true,
                                                   stream_callback_mod => grpcbox_client_stream}) of
         {ok, Conn} ->
