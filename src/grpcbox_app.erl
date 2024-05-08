e2cdc9399be7bc35055e6fafa419d85afa2d11aa
AlexanderRyblov <a.ryblov@efg.gg>
2024-05-08 17:36:58 +0300

    debug

--- src/grpcbox_app.erl
+++ src/grpcbox_app.erl
@@ -15,9 +15,11 @@ start(_StartType, _StartArgs) ->
     {ok, Pid} = grpcbox_sup:start_link(),
     case application:get_env(grpcbox, client) of
         {ok, #{channels := Channels}} ->
+            ?LOG_INFO(#{what => debug_grpcbox_app_start_1, channels => Channels}),
             [grpcbox_channel_sup:start_child(Name, Endpoints, Options)
              || {Name, Endpoints, Options} <- Channels];
         _ ->
+            ?LOG_INFO(#{what => debug_grpcbox_app_start_2}),
             ok
     end,
 
