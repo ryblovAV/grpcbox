c3323f5718c0d3f68e320cb6aa27a36d0ee566e6
AlexanderRyblov <a.ryblov@efg.gg>
2024-05-08 16:51:03 +0300

    debug

--- src/grpcbox_name_resolver.erl
+++ src/grpcbox_name_resolver.erl
@@ -2,16 +2,21 @@
 
 -export([resolve/1]).
 
+-include_lib("kernel/include/logger.hrl").
+
 %% dns:///localhost
 %% ipv4:///127.0.0.1
 
 resolve(Name) ->
+    ?LOG_INFO(#{what => debug_grpcbox_name_resolver_0}),
     case uri_string:parse(Name) of
         #{scheme := _Scheme,
           host := _Authority,
           port := Port,
           path := Endpoint} ->
-            {Endpoint, Port};
+          ?LOG_INFO(#{what => debug_grpcbox_name_resolver_1}),
+          {Endpoint, Port};
         _ ->
-            {"127.0.0.1", 8080}
+          ?LOG_INFO(#{what => debug_grpcbox_name_resolver_2}),
+          {"127.0.0.1", 8080}
     end.
